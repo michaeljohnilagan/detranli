ef91c49ac94add3dc043b7a2803f0630799ed575
Michael John Ilagan <michael.ilagan@mail.mcgill.ca>
2024-04-08 11:13:02 -0400

    created dataset

---
+++ R/cnrexample1.R
@@ -0,0 +1,10 @@
+#' Simulated dataset with random uniform responses.
+#'
+#' A simulated dataset with uniform random responses.
+#' The first 75 rows are fully attentive.
+#' The remaining 225 rows have all item responses independently drawn from a 
+#' uniform distribution.
+#'
+#' @format
+#' A data frame with 300 rows and 18 columns.
+"cnrexample1"
